04d6e9e957fa599b4790a8fe84d6d3bc9a026d27
Rustam Kalimulin <kalimulin-ru@yandex.ru>
2018-04-10 21:44:25 +0400

    gallery

--- src/components/RelatedItems/RelatedItems.vue
+++ src/components/RelatedItems/RelatedItems.vue
@@ -11,25 +11,27 @@
             .related-items__item-collection New Swimwear Collection
           swiper-slide
             img.related-items__slide-image(src="../../assets/images/rectangle2.png")
-            .related-items__item-name BATHING SUIT 1 — $55
+            .related-items__item-name BATHING SUIT 2 — $255
             .related-items__item-collection New Swimwear Collection
           swiper-slide
             img.related-items__slide-image(src="../../assets/images/rectangle2.png")
-            .related-items__item-name BATHING SUIT 1 — $55
+            .related-items__item-name BATHING SUIT 3 — $355
             .related-items__item-collection New Swimwear Collection
           swiper-slide
             img.related-items__slide-image(src="../../assets/images/rectangle2.png")
-            .related-items__item-name BATHING SUIT 1 — $55
+            .related-items__item-name BATHING SUIT 4 — $455
             .related-items__item-collection New Swimwear Collection
           swiper-slide
             img.related-items__slide-image(src="../../assets/images/rectangle2.png")
-            .related-items__item-name BATHING SUIT 1 — $55
+            .related-items__item-name BATHING SUIT 5 — $555
             .related-items__item-collection New Swimwear Collection
           swiper-slide
             img.related-items__slide-image(src="../../assets/images/rectangle2.png")
-            .related-items__item-name BATHING SUIT 1 — $55
+            .related-items__item-name BATHING SUIT 6 — $655
             .related-items__item-collection New Swimwear Collection
       .related-items__arrow-right next
+    .related-items__view-item.double-button
+      .double-button__inner view item
 
 
 </template>
@@ -57,7 +59,8 @@
           },
           slidesPerView: 3,
           centeredSlides: true,
-          spaceBetween: 50
+          spaceBetween: 50,
+          initialSlide: 1
         }
       }
     }
